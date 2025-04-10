e9d0bba3726524b276da56c2af8e2dc95aa7fc52
mohamedSayedBayoumy <mandobobo12345@gmail.com>
2025-04-10 12:34:52 +0200

    config and google map ..

--- lib/home.dart
+++ lib/home.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(31, 41),
+        ),
+      ),
+    );
   }
 }
